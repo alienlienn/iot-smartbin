dab1c1e3c28c0a7398c9bc92f9f2e87902212ce9
a_liennn <lientoh@gmail.com>
2025-03-01 05:21:24 +0800

    sidebar + standardized css

--- SmartBin/Dashboard/urls.py
+++ SmartBin/Dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.dashboard, name="dashboard"), 
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
